5ba0aad2a9b26935a1b0eb06fd612d2cdf45630a
Dariusz Okoński <darek200180@gmail.com>
2025-06-09 12:15:16 +0200

    add component List

---
+++ src/components/List/List.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const List = () => {
+  return (
+    <div>
+      <header>
+        Things to do <span>soon</span>
+      </header>
+      <p>Interesting things I want to check out</p>
+
+      <section>
+        <article>Books</article>
+        <article>Movies</article>
+        <article>Games</article>
+      </section>
+    </div>
+  );
+};
+
+export default List;
