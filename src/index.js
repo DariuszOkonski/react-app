ed56c3454204b0c934bbbf69769e2c7e59451841
Dariusz Okoński <darek200180@gmail.com>
2025-06-18 19:58:46 +0200

    add store to Provider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
