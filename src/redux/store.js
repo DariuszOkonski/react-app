a46d4386c42a8853a8293504e293881fa4a12931
Dariusz Okoński <darek200180@gmail.com>
2025-06-23 19:13:45 +0200

    add function getFilteredCards

--- src/redux/store.js
+++ src/redux/store.js
@@ -31,3 +31,10 @@ const store = createStore(
 );
 
 export default store;
+
+export const getFilteredCards = (state, columnId) =>
+  state.cards.filter(
+    (card) =>
+      card.columnId === columnId &&
+      card.title.toLowerCase().includes(state.searchString.toLowerCase())
+  );
