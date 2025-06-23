a24c018226ea1e365633ee350e1178224fd590d1
Dariusz Okoński <darek200180@gmail.com>
2025-06-23 19:45:54 +0200

    code rafactor

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux';
+import { getFilteredCards } from '../../redux/store';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
 import styles from './Column.module.scss';
-import { getFilteredCards } from '../../redux/store';
 
 const Column = (props) => {
   const cards = useSelector((state) => getFilteredCards(state, props.id));
