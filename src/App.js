9ceefd51172f49c186556358a6f2bd1a41f0fc23
Dariusz Okoński <darek200180@gmail.com>
2025-06-24 20:22:21 +0200

    add About and Favorite to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Container from './components/Container/Container';
 import NavBar from './components/NavBar/NavBar';
+import About from './pages/About/About';
+import Favorite from './pages/Favorite/Favorite';
 import Home from './pages/Home/Home';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <NavBar />
       <Container>
         <Home />
+        <About />
+        <Favorite />
       </Container>
     </main>
   );
