c24ea5fa923ff6500b3fe40dca2089f4ed419c5d
Dariusz Okoński <darek200180@gmail.com>
2025-06-24 19:59:26 +0200

    add pages About and Favorite

---
+++ src/pages/About/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit
+        perferendis cupiditate dolores doloribus nobis quia voluptas dignissimos
+        quod et iste dolor vel ipsam delectus fugit rerum, maxime vitae. Fugit,
+        ipsum quo a voluptatem, natus, consequuntur alias sint eveniet aliquid
+        fugiat laboriosam obcaecati pariatur neque magnam? Nemo expedita eveniet
+        exercitationem minus.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Favorite/Favorite.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Favorite() {
+  return (
+    <div>
+      <h1>Favorite</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam,
+        laudantium iste incidunt eius mollitia, assumenda iure maiores cumque
+        possimus nisi repellat, corrupti nam impedit nihil eveniet inventore
+        praesentium molestiae soluta.
+      </p>
+    </div>
+  );
+}
+
+export default Favorite;
