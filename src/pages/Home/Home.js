903878906ed9048678c8ce412894b96f566611ca
Dariusz Okoński <darek200180@gmail.com>
2025-06-25 21:38:11 +0200

    remove list from Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
 import SearchForm from '../../components/SearchForm/SearchForm';
-import List from '../../components/List/List';
 import Lists from '../../components/Lists/Lists';
 
 function Home() {
